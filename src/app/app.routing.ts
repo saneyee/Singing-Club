f52e712b2c43728058e7a5f244da4baa0d3f1ce9
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-12-17 21:13:02 -0800

    updates styles.css

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,20 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { EventsComponent } from './events/events.component';
+import { AdminComponent }   from './admin/admin.component';
+
 
 const appRoutes: Routes = [
-  {
-     path: '',
-     component: WelcomeComponent
-   },
-  {
-    path: 'about',
-    component: AboutComponent
- },
-  {
-    path: 'events',
-    component: EventsComponent
-  }
- ];
+    {
+        path: '',
+        component: WelcomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'events',
+        component: EventsComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
