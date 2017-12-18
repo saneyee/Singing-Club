3603463c06a3a784addf18850f3c3acd6e0c0ef4
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-12-17 20:17:45 -0800

    api keys are added

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,7 +1,8 @@
 export var masterFirebaseConfig = {
-    apiKey: "xxxx",
-    authDomain: "xxxx.firebaseapp.com",
-    databaseURL: "https://xxxx.firebaseio.com",
-    storageBucket: "xxxx.appspot.com",
-    messagingSenderId: "xxxx"
+    apiKey: "AIzaSyDvup1aceLIXCvMp9QFn9w3VSmLOVJNc14",
+    authDomain: "singing-club.firebaseapp.com",
+    databaseURL: "https://singing-club.firebaseio.com",
+    projectId: "singing-club",
+    storageBucket: "singing-club.appspot.com",
+    messagingSenderId: "86385883953"
   };
